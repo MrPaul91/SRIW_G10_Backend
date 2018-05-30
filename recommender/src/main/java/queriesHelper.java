d93e4a2f560d1cc8c8084447d2e9fd8241621b86
Juan Pablo Villegas Gómez <jupvillegasgo@unal.edu.co>
2018-05-30 16:30:28 -0500

    Commit 3

--- recommender/src/main/java/queriesHelper.java
+++ recommender/src/main/java/queriesHelper.java
@@ -1,4 +1,25 @@
+import org.apache.jena.query.ResultSet;
+import virtuoso.jena.driver.*;
+import org.apache.jena.query.*;
 
-public class queriesHelper {
+
+public class queriesHelper{
+	
+	
+	static ResultSet runQuery(String consulta, String nameOfGraph)
+	{
+		Query sparql = QueryFactory.create(consulta);
+		
+		VirtGraph set = new VirtGraph(nameOfGraph,"jdbc:virtuoso://50.18.123.50:1111","dba","dba");
+		
+		VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create(sparql, set);
+	    
+	    ResultSet results = vqe.execSelect();
+	    
+	    return results;
+	    
+	    
+	}
 
 }
+
