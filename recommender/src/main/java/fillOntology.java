30686c6be3602cb187d8c63d59c9f04136751f23
jbyepez <jbyepezh@unal.edu.co>
2018-05-31 16:34:43 -0500

    comprobar login mediante GET
    se cambiara a POST proximamente

--- recommender/src/main/java/fillOntology.java
+++ recommender/src/main/java/fillOntology.java
@@ -19,7 +19,7 @@ public class fillOntology {
 		
 		port(8008);
 		
-		VirtGraph set = new VirtGraph("company","jdbc:virtuoso://50.18.123.50:1111","dba","dba");
+		//VirtGraph set = new VirtGraph("company","jdbc:virtuoso://50.18.123.50:1111","dba","dba");
 		
 		
 		
@@ -29,14 +29,28 @@ public class fillOntology {
 		
 		BufferedReader reader = new BufferedReader(file);
 	    String s = reader.readLine();*/
-	    
+
+		/**
 	    String query = "PREFIX ds:<https://www.datos.gov.co/resource/yigb-eqpm/> "
 	    		+ "SELECT ?direccion ?sector ?nombreinstitucion WHERE "
 	    		+ "{ ?x ds:nombre_de_la_instituci_n ?nombreinstitucion . "
 	    		+ "?x ds:direcci_n ?direccion. ?x ds:sector ?sector }";
-	    		
-	    ResultSet results = queriesHelper.runQuery(query, "company");
-	    
+	    /**/
+
+		/**/
+		String query = "PREFIX ds:<http://www.entrega1/ontologies/> \n" +
+				"PREFIX dbo:<http://dbpedia.org/ontology/>\n" +
+				"SELECT DISTINCT ?direccion ?sector ?nombreinstitucion\n" +
+				"WHERE { \n" +
+				"?x ds:companyName ?nombreinstitucion .\n" +
+				"?x ds:hasSector ?sector .\n" +
+				"?x dbo:address ?direccion\n" +
+				"}\n" +
+				"order by asc(?x)";
+	    /**/
+
+	    ResultSet results = queriesHelper.runQuery(query, "companies");
+
 	    List<Company> list = new ArrayList<Company>();
 	   
 	   while (results.hasNext()){
@@ -84,7 +98,22 @@ public class fillOntology {
 			System.out.print("entro a pedir este mostro");
 			return list;
 		}, gson ::toJson);
-	   
+
+	   get("/login/:id/:pass", (request, response) -> {
+	   	String loginQuery = "PREFIX ds:<http://www.entrega1/ontologies/> \n" +
+				"PREFIX dbo:<http://dbpedia.org/ontology/>\n" +
+				"SELECT DISTINCT ?id ?password\n" +
+				"WHERE { \n" +
+				"?x ds:companyId \"" + request.params(":id") + "\" .\n" +
+				"?x ds:companyPassword \"" + request.params(":pass") + "\"\n" +
+				"}\n";
+
+	   	ResultSet resultsLogin = queriesHelper.runQuery(loginQuery, "companies");
+
+	   	if(resultsLogin.hasNext()) return "true";
+	   	return "id " + request.params(":id")+" or pass "+request.params(":pass") + " are incorrects.";
+	   });
+
 	   System.out.println("hola");
     
     }
